1fc64a64b601e0fe8e41f167c00c5d9e27000bb7
Jan-Willem Spuij <jwspuij@gmail.com>
2013-10-10 18:44:29 +0200

    - Implemented OpenThermOEMDiagnosticCodeFrame - Implemented OpenThermOEMDiagnosticCodeFrameTest

---
+++ bundles/binding/org.openhab.binding.opentherm.test/src/test/java/org/openhab/binding/opentherm/internal/protocol/frame/OpenThermControl2ndSetpointFrameTest.java
@@ -0,0 +1,57 @@
+/**
+ * openHAB, the open Home Automation Bus.
+ * Copyright (C) 2010-2013, openHAB.org <admin@openhab.org>
+ *
+ * See the contributors.txt file in the distribution for a
+ * full listing of individual contributors.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation; either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, see <http://www.gnu.org/licenses>.
+ *
+ * Additional permission under GNU GPL version 3 section 7
+ *
+ * If you modify this Program, or any covered work, by linking or
+ * combining it with Eclipse (or a modified version of that library),
+ * containing parts covered by the terms of the Eclipse Public License
+ * (EPL), the licensors of this Program grant you additional permission
+ * to convey the resulting work.
+ */
+package org.openhab.binding.opentherm.internal.protocol.frame;
+
+import static org.junit.Assert.*;
+
+import java.math.BigDecimal;
+
+import org.junit.Test;
+import org.openhab.binding.opentherm.internal.protocol.serial.SerialMessage;
+
+/**
+ * Tests the {@link OpenThermControlSetpointFrame} class.
+ *
+ * @author Jan-Willem Spuij <jwspuij@gmail.com>
+ * @since 1.4.0
+ *
+ */
+public class OpenThermControl2ndSetpointFrameTest {
+
+	/**
+	 * Test method for {@link org.openhab.binding.opentherm.internal.protocol.frame.OpenThermControlSetpointFrame#getControlSetpoint()}.
+	 */
+	@Test
+	public void testGetControlSetpoint() {
+		SerialMessage serialMessage = new SerialMessage("BC0081402");
+		OpenThermControl2ndSetpointFrame frame = (OpenThermControl2ndSetpointFrame) OpenThermFrame.fromSerialMessage(serialMessage);
+		assertEquals(0, frame.getControlSetpoint().compareTo(new BigDecimal("20.01")));
+	}
+
+}
